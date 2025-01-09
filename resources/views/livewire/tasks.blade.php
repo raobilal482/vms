d55af15d6bd01ac34e12d8b41626f00c77a278b9
bilalrao482 <raobilal482@gmail.com>
2025-01-09 14:20:10 +0500

    routes

---
+++ resources/views/livewire/tasks.blade.php
@@ -0,0 +1,3 @@
+<div>
+thsi is task management
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Livewire\AdminPanelHome;
 use App\Livewire\Dashboard;
+use App\Livewire\Events;
+use App\Livewire\Tasks;
+use App\Livewire\Time;
+use App\Livewire\Volunteer;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -17,4 +21,9 @@ Route::view('profile', 'profile')
 require __DIR__.'/auth.php';
 
 Route::middleware(['auth'])->get('/admin', AdminPanelHome::class)->name('admin.panel');
+Route::middleware(['auth'])->get('/volunteer', Volunteer::class)->name('volunteer');
+Route::middleware(['auth'])->get('/events', Events::class)->name('events');
+Route::middleware(['auth'])->get('/tasks', Tasks::class)->name('tasks');
+Route::middleware(['auth'])->get('/time', Time::class)->name('time');
+
 Route::middleware(['auth'])->get('/dashboard', Dashboard::class)->name('dashboard');
